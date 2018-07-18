f1bac2a5e897727b54aeb66bc7c44985868b1ddf
alex-chischevoy <chischevoyalex@gmail.com>
2018-07-18 19:47:05 +0300

    Задание 3

---
+++ src/com/example/task/Task3.java
@@ -0,0 +1,11 @@
+package com.example.task;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println(sum(4, 5));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
