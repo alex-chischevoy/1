1f2b5febd3fd07cd9a3e51e31dd22f1cb02116ed
alex-chischevoy <chischevoyalex@gmail.com>
2018-07-18 18:50:26 +0300

    бонус задания 3

--- src/com/example/task/Three.java
+++ src/com/example/task/Three.java
@@ -1,7 +1,31 @@
 package com.example.task;
 
+import java.util.Scanner;
+
 public class Three {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите три числа");
+        int first = scn.nextInt();
+        int second = scn.nextInt();
+        int third = scn.nextInt();
+        if (first > second && first > third) {
+            System.out.println("Наибольшее число: " + first);
+        }
+        if (first < second && first < third) {
+            System.out.println("Наименьшее число: " + first);
+        }
+        if (second > first && second > third) {
+            System.out.println("Наибольшее число: " + second);
+        }
+        if (third < second && third < first) {
+            System.out.println("Наименьшее число: " + third);
+        }
+        if (third > first && third > second) {
+            System.out.println("Наибольшее число: " + third);
+        }
+        if (second < first && second < third) {
+            System.out.println("Наименьшее число: " + second);
+        }
     }
 }
